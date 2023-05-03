ddaadd8f8b80041bff9bcdb03f2492e06a8a6ad1
jean <jean@glowforthgames.com>
2023-05-02 21:47:46 -0400

    sixth

--- components/card/InputFilterBox.js
+++ components/card/InputFilterBox.js
@@ -20,5 +20,16 @@ const InputFilterBox = ({placeholder,value, setValue, type}) => {
         />
     );
 }
+const InputFilterBox3 = ({placeholder3,value, setValue, type}) => {
+    return (
+        <input
+            type={type}
+            value={value}
+            onChange={(e)=>setValue(e.target.value)}
+            placeholder3={placeholder3}
+            className="bg-transparent border-2 border-solid border-customGray-400 outline-none py-2 px-5 text-sm leading-6 text-[#373737] font-normal font-sans rounded-[10px] w-full"
+        />
+    );
+}
 
 export default InputFilterBox;
