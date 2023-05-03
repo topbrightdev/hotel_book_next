278ba13dd10b5056c3798c14d2f7e5f591d24a11
jean <jean@glowforthgames.com>
2023-05-02 21:39:18 -0400

    third commit

--- components/card/InputFilterBox.js
+++ components/card/InputFilterBox.js
@@ -1,9 +1,9 @@
-const InputFilterBox2 = ({placeholder1,Value2, setValue2, type}) => {
+const InputFilterBox2 = ({placeholder1,value, setValue, type}) => {
     return (
         <input
             type={type}
-            value={Value2}
-            onChange={(e)=>setValue2(e.target.Value2)}
+            value={value}
+            onChange={(e)=>setValue(e.target.value)}
             placeholder1={placeholder1}
             className="bg-transparent border-2 border-solid border-customGray-400 outline-none py-2 px-5 text-sm leading-6 text-[#373737] font-normal font-sans rounded-[10px] w-full"
         />
